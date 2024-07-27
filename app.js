1e159a679444e2dde3ac3ce48f087036d998d28c
tawhid2001 <tawhid.talal@gmail.com>
2024-07-27 13:13:34 -0400

    done

--- app.js
+++ app.js
@@ -1,21 +1,19 @@
-const getUserDetails =()=>{
-    const token = localStorage.getItem('authToken');
-    
-  fetch('https://enlighten-institute.onrender.com/api/auth/user/', {
-      method: 'GET',
-      headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Token ${token}`
-      }
-  })
-  .then(response => response.json())
-  .then(data => {
-   
-    console.log(data);
-      localStorage.setItem("user_type",data.user_type);
-      localStorage.setItem("user_id",data.id);
-  });
-  };
+const getUserDetails = () => {
+  const token = localStorage.getItem("authToken");
 
-  getUserDetails();
-  
+  fetch("https://enlighten-institute.onrender.com/api/auth/user/", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Token ${token}`,
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      localStorage.setItem("user_type", data.user_type);
+      localStorage.setItem("user_id", data.id);
+    });
+};
+
+getUserDetails();
