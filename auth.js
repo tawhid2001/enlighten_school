a4353d4c83f416e531c4cc8c0a2ee11a1f726d2f
tawhid2001 <tawhid.talal@gmail.com>
2024-07-27 11:52:56 -0400

    app.js

--- auth.js
+++ auth.js
@@ -55,9 +55,6 @@ const handleLogin = (event) => {
         console.log("Login successful:", data);
         localStorage.setItem("authToken", data.key);
         window.location.href = "./index.html"; // Redirect to dashboard or home page
-        setTimeout(() => {
-            window.location.reload();
-        }, 100);
     })
     .catch(error => {
         console.error('Login error:', error.message);
